4c5a22ea2aa73762439db6d4b1a6241a30803ee0
Hanh109 <96662137+Hanh109@users.noreply.github.com>
2022-01-15 16:16:52 +0700

    Add files via upload

---
+++ Bootstrap1/1.js
@@ -0,0 +1,23 @@
+﻿ $(function(){
+    //  $('.lentren').click(function(){
+    //      $('body').animate({scrollTop:0});
+    //      return false;
+    //  })
+   $(window).scroll(function(){
+    var pos_body = $('html,body').scrollTop();
+    //    if($('body').scrollTop()>400)
+    //    {
+        if(pos_body>200){ 
+       $('.logo').addClass('logodoimau')
+       $('.chumenu ').addClass('logodoimau')
+       $('.menutren').addClass('nenmenu')
+       }
+       else 
+       {
+        $('.logo').removeClass('logodoimau')
+        $('.chumenu ').removeClass('logodoimau')
+        $('.menutren').removeClass('nenmenu')
+       }
+   })
+})  
+ 
